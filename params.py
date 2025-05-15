4677666438dde3dfc8172c838e94fdab55b5edf3
michiel-j <72069547+michiel-j@users.noreply.github.com>
2025-05-15 23:23:24 +0200

    Add plots

--- params.py
+++ params.py
@@ -11,5 +11,5 @@ learning_rate_no_dann = 1e-3 # Copied from https://adapt-python.github.io/adapt/
 learning_rate_dann = 1e-3
 beta1 = 0.5 # Copied from https://adapt-python.github.io/adapt/examples/Two_moons.html#Source-Only
 beta2 = 0.999 # The default in both TensorFlow and PyTorch ADAM
-weight_decay=0 #1e-4
+weight_decay = 0
 domain_adaptation_lambda = 1.0 # In range [0, 1], copied from https://adapt-python.github.io/adapt/examples/Two_moons.html#DANN
